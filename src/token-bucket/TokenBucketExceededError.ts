b55f47470ea76b08ce7fb566cd8eaa245add6f42
efuturetoday <oliver.koeln@gmail.com>
2017-06-02 22:34:52 +0200

    fix(token-bucket): Now respects message for TokenBucketExceededError.

--- src/token-bucket/TokenBucketExceededError.ts
+++ src/token-bucket/TokenBucketExceededError.ts
@@ -1,8 +1,4 @@
 import { ExtendableError } from '../ExtendableError';
 
 export class TokenBucketExceededError extends ExtendableError {
-
-    constructor(message: string) {
-        super('Can not exeed bucketSize!');
-    }
 }
