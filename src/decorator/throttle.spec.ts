4bf37f65f9116ef14422b24d45e84f6df8364305
Oliver Haase <oliver.koeln@gmail.com>
2017-06-08 17:12:47 +0200

    Implemented some test cases for throttle-decorator

--- src/decorator/throttle.spec.ts
+++ src/decorator/throttle.spec.ts
@@ -1,46 +1,86 @@
+import { Channels } from '../channel/Channels';
+import { ChannelNotFoundError } from './ChannelNotFoundError';
 import { throttle } from './throttle';
+import { expect } from 'chai';
+import { useFakeTimers, SinonFakeTimers, spy } from 'sinon';
 
 describe('Decorator: throttle', function () {
-    it('should attach to async function', function () {
-        class A {
-            @throttle('channel1')
-            async foo() {
-            }
-        }
+
+    let clock: SinonFakeTimers;
+    before(function () {
+        clock = useFakeTimers();
     });
 
-    it('should attach to promise', function () {
+    after(function () {
+        clock.restore();
+    });
+
+    beforeEach(function () {
+        Channels.channels.clear();
+    });
+
+    it('should attach to async function', function () {
+        let channelId = 'testChannel';
+        let channel = Channels.createSimple(channelId, 1, 'second');
+
+        let mock = spy(channel, 'enqueueTask');
         class A {
-            @throttle('channel1')
-            foo(): Promise<any> {
+            @throttle(channelId)
+            static async foo() {
+            }
+        }
+
+        A.foo();
+        mock.should.have.been.calledOnce;
+    });
+
+    it('should attach to Promise', function () {
+        let channelId = 'testChannel';
+        let channel = Channels.createSimple(channelId, 1, 'second');
+
+        let mock = spy(channel, 'enqueueTask');
+        class A {
+            @throttle(channelId)
+            static foo(): Promise<any> {
                 return new Promise((resolve, reject) => {
                     resolve();
                 });
             }
         }
+
+        A.foo();
+        mock.should.have.been.calledOnce;
     });
 
-    it('should use the simple options', function() {
-
-    });
-
-    it('should use the advanced options', function() {
-
-    });
-
-    it('should throw if channel is not found', function() {
+    it('should use the simple options', function () {
 
     });
 
-    it('should allow burst', function() {
+    it('should use the advanced options', function () {
 
     });
 
-    it('should delay on drain', function() {
+    it('should throw if channel is not found', function () {
+        class A {
+            @throttle('unknown')
+            static async foo() {
+            }
+        }
+
+        A.foo().catch(function(err) {
+            err.should.be.instanceof(ChannelNotFoundError);
+        });
+    });
+
+    it('should allow burst', function () {
 
     });
 
-    it('should respect ttl', function() {
+    it('should delay on drain', function () {
 
+    });
+
+    it('should respect ttl', function () {
+        
     });
 });
